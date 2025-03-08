b6fdad0594b7aec6c0fe695bf61ada9833dfed67
chanios <chanios@proton.me>
2025-03-09 00:10:32 +0700

    update

--- src/database.php
+++ src/database.php
@@ -9,6 +9,7 @@ $password = getenv("DB_PASSWORD") ?: "your_password";
 function getDbConnection()
 {
     try {
+        global $host, $user, $password, $dbname;
         $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
